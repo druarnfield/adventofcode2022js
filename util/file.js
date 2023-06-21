6386a8e8f17daf55c47c14f41d35fad934ce608e
Dru Arnfield <druarnfield@gmail.com>
2023-06-21 20:42:17 +1000

    Added a solution for Day 2. decided to experiment with if else statements instead of a switch statement to calculate the scores. I think it looks cleaner and is more readable. I also added a file.js utils to handle reading the input files.

---
+++ util/file.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+
+function readFile(inputPath){
+    try {
+        const data = fs.readFileSync(inputPath, 'utf8');
+        return data.split('\n');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = { readFile };
